58f8378b374469b228c9663d64e7bd6081ebb50c
Chirayu <chirayupancholi17@gmail.com>
2023-01-29 07:36:07 +0530

    Created Vite App and completed nav-bar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className='mb-2 h-16 md:border-b '>
+            <div className='container mx-auto '>
+                <div className='flex justify-between gap-2 mx-2 py-2'>
+                    <h1 className='text-black text-xl sm:text-2xl md:text-3xl font-semibold md:pl-8'>
+                        Budget
+                    </h1>
+                    <div className='flex justify-between gap-4 sm:gap-16'>
+                        <button className='bg-blue-600 p-1 sm:p-2 text-white text-sm sm:text-lg md:text-xl '>
+                            Add Expenses
+                        </button>
+                        <button className='bg-white border-solid border-2 border-grey hover:border-blue-600 p-1 sm:p-2 text-blue-600 text-sm sm:text-lg md:text-xl sm:mr-6 md:mr-10'>
+                            Add Budget
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Navbar
